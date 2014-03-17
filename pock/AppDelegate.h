69518cd7baadd4a7ad2edbc4bb8be3eb3bf0b3e7
runcrow <runcrow@runcrows-Mac.local>
2014-03-17 23:36:51 +0800

    Initial Commit

---
+++ pock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pock
+//
+//  Created by runcrow on 14-3-17.
+//  Copyright (c) 2014年 runcrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pock
+//
+//  Created by runcrow on 14-3-17.
+//  Copyright (c) 2014年 runcrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
